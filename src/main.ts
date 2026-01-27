b03e7f1902c7e3dceadfba25c4ca2c012cee1966
Fabián Morag <fabian.morag@innovex.cl>
2026-01-27 16:27:20 -0300

    feat: enable CORS for the Sakila API

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory);
 
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
