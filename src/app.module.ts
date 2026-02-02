475d245a387a80c65eedaf92dc68be50109e5b49
Fabián Morag <fabian.morag@innovex.cl>
2026-02-02 09:41:24 -0300

    feat: remove ReportsModule from AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,12 @@ import { AppService } from './app.service';
 import { FilmsModule } from './films/films.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
-import { ReportsModule } from './reports/reports.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     FilmsModule,
     PrismaModule,
-    ReportsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
