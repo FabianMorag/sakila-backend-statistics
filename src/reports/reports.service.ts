6a59882eba91b567eda859356ee2e58e971716ea
Fabián Morag <fabian.morag@innovex.cl>
2026-02-06 09:46:31 -0300

    feat: add Reports module with controller and service for top categories

---
+++ src/reports/reports.service.ts
@@ -0,0 +1,26 @@
+import { PrismaService } from '@/prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ReportsService {
+  constructor(private prisma: PrismaService) {}
+
+  topCategories() {
+    return this.prisma.$queryRaw<
+      Array<{ category_id: number; name: string; total_revenue: number }>
+    >`
+      SELECT 
+        c.name,
+        SUM(p.amount) as total_revenue
+      FROM category c
+      INNER JOIN film_category fc ON c.category_id = fc.category_id
+      INNER JOIN film f ON fc.film_id = f.film_id
+      INNER JOIN inventory i ON f.film_id = i.film_id
+      INNER JOIN rental r ON i.inventory_id = r.inventory_id
+      INNER JOIN payment p ON r.rental_id = p.rental_id
+      GROUP BY c.category_id, c.name
+      ORDER BY total_revenue DESC
+      LIMIT 5
+    `;
+  }
+}
