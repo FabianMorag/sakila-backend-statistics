19b7dada8bbdcf68e0226c0cf7edb12eb83c7196
Fabián Morag <fabian.morag@innovex.cl>
2026-02-17 09:51:23 -0300

    feat: update getHello method to return a welcome HTML message for the Sakila API

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,15 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const HTMLDivElement = `
+    <section style="min-height:100dvh;align-content:center;">
+      <div style="margin:auto;width:fit-content;display:grid;place-items:center;gap:1rem;">
+        <h1>Welcome to the Sakila API!</h1>
+        <p>This is a simple API built with NestJS.</p>
+        <a href="/swagger">Go to swagger</a>
+      </div>
+    </section>
+    `;
+    return HTMLDivElement;
   }
 }
